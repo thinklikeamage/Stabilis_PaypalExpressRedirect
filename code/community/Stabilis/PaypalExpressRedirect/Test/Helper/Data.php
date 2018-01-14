531bf14ef47b41f52001b75615ecefc33d30ce53
Luke Leber <lal65@psu.edu>
2018-01-14 13:11:49 -0500

    Updated structure?

--- code/community/Stabilis/PaypalExpressRedirect/Test/Helper/Data.php
+++ code/community/Stabilis/PaypalExpressRedirect/Test/Helper/Data.php
@@ -2,6 +2,13 @@
 
 class Stabilis_PaypalExpressRedirect_Test_Helper_Data extends EcomDev_PHPUnit_Test_Case {
 	
+	protected $_helper;
+
+    protected function setUp() {
+		parent::setUp();
+		$this->_helper = Mage::helper('stabilis_paypalexpressredirect');
+	}
+	
 	public function testThing() {
 		$this->assertTrue(true);
 	}
